b293c3c768392af05dbd7d289acce6a51df3dd8c
JaydenTeknorix <jayden.ferrao@teknorix.com>
2026-06-03 09:52:07 +0530

    Minor change

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import IssueFeedScreen from './screens/IssueFeedScreen';
 import IssueDetailScreen from './screens/IssueDetailScreen';
 import ReportMapScreen from './screens/ReportMapScreen';
